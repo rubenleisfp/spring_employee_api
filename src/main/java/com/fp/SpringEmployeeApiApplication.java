54ef2cb62f898ed8d33a655384dd6a5cdaed24db
Ruben Leis <rubenleisfp@gmail.com>
2025-10-06 11:11:45 +0200

    Progresamos a una version estable

--- src/main/java/com/fp/SpringEmployeeApiApplication.java
+++ src/main/java/com/fp/SpringEmployeeApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringEmployeeApiApplication {
 
+	//http://localhost:8080/swagger-ui/index.html
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEmployeeApiApplication.class, args);
 	}
